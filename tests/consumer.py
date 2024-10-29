632de708ef7a9be5bfaf5bfe070e0fdbd1e7c1bd
dushes <dushes.nadym@gmail.com>
2024-10-29 18:59:36 +0800

    consumer

---
+++ tests/consumer.py
@@ -0,0 +1,23 @@
+import asyncio
+from kafka_avro_helper.validate import validate_schemas
+from tests.slow_qraphql_operation_detected import SlowQraphqlOperationDetected
+from tests.user_feedback import UserFeedback
+from kafka_avro_helper.consumer import consume_messages, KafkaMessage
+
+
+async def process_user_feedback(
+        value: UserFeedback | SlowQraphqlOperationDetected,
+        topic: str,
+        key: str,
+        **kwargs
+) -> [KafkaMessage]:
+    return []
+
+
+async def main():
+    await validate_schemas(consume_schemas=[UserFeedback, SlowQraphqlOperationDetected])
+    await consume_messages(callback=process_user_feedback)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
